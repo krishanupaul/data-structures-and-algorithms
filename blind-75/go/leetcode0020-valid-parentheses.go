7e1b65fd42cfa278c50102260714a0aa79803908
Krishanu Paul <krishanupaul7@gmail.com>
2026-08-02 14:15:37 +0530

    valid parenthesis completed for GO

--- blind-75/go/leetcode0020-valid-parentheses.go
+++ blind-75/go/leetcode0020-valid-parentheses.go
@@ -4,5 +4,20 @@
 package main
 
 func isValid(s string) bool {
-    
+	var stack = make([]rune, 0, len(s))
+
+	for _, currChar := range s {
+		stackLen := len(stack)
+
+		if stackLen > 0 {
+			top := stack[stackLen-1]
+			if (currChar == ')' && top == '(') || (currChar == ']' && top == '[') || (currChar == '}' && top == '{') {
+				stack = stack[:stackLen-1]
+				continue
+			}
+		}
+		stack = append(stack, currChar)
+	}
+
+	return len(stack) == 0
 }
