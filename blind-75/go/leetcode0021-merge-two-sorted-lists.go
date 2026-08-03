3f5c2222e0af307bb7ad86a55d26f05cb0684ea1
Krishanu Paul <krishanupaul7@gmail.com>
2026-08-03 22:17:10 +0530

    Added solution for merge two lists

--- blind-75/go/leetcode0021-merge-two-sorted-lists.go
+++ blind-75/go/leetcode0021-merge-two-sorted-lists.go
@@ -4,5 +4,26 @@
 package main
 
 func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
-    
+	mergeNode := &ListNode{}
+	newList := mergeNode
+
+	for list1 != nil || list2 != nil {
+		if list1 == nil {
+			mergeNode.Next = list2
+			list2 = list2.Next
+		} else if list2 == nil {
+			mergeNode.Next = list1
+			list1 = list1.Next
+		} else if list1.Val > list2.Val {
+			mergeNode.Next = list2
+			list2 = list2.Next
+		} else {
+			mergeNode.Next = list1
+			list1 = list1.Next
+		}
+
+		mergeNode = mergeNode.Next
+	}
+
+	return newList.Next
 }
